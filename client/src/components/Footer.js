07c38766e6cd8388b8ac383e330498a5cdeaedd0
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-04-02 21:34:51 +0530

    updated

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,18 +7,3 @@ export class Footer extends  Component {
         super(props);
     }
 
-    render() {
-        return(
-<div class="footer">
-            <div className="d-flex align-items-center text-center">
-                <div className="footer-text">Copyright © 2023</div>
-                <div className="footer-text">HodlInfo.com</div>
-                <div className="footer-text">Developed By Ankit Kumar Verma</div>
-                <div class="footer-text pointer" style={{ "margin-left": "auto" }}>
-                    <a href="mailto:support@hodlinfo.com" class="footer-text-link">Support</a>
-                </div>
-            </div>
-</div>
-        )
-    }
-}
